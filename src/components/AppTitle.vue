87cf2fdb5e5dae4749d255719020cb6768a2cf40
Emilio Gallo <emilio.gallo17@gmail.com>
2024-04-30 17:53:23 +0200

    add: title component to app - html&js

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<script>
+
+   export default {
+
+      data() {
+         return {
+
+            title: "My first app with Vite!"
+
+         };
+      },
+
+   };
+
+</script>
+
+<template>
+
+   <h1>{{ title }}</h1>
+
+</template>
+
+<style>
+
+   /*  */
+
+</style>
